dfaf9a5d78064a7a78c19e9a117529d02290152c
marongzhen <2712268578@qq.com>
2023-08-26 17:55:17 +0800

    feat(custom): 修改了日程表选项菜单展开功能，PC端右键展开，移动端点击展开

--- src/view/home/calendar/v.vue
+++ src/view/home/calendar/v.vue
@@ -31,7 +31,27 @@
     }
   }
 
-  .panel {
+  .panelPC {
+    position: fixed;
+    top: v-bind(contextMenuTop);
+    left: v-bind(contextMenuLeft);
+    width: 10%;
+    height: 20%;
+    background-color: var(--color-least);
+    transition: bottom 0.5s ease-in-out;
+
+    @include flex(column, start, center);
+
+    .close,
+    .name {
+      margin: 4px;
+      font-size: 14px;
+      color: var(--color-main);
+      font-weight: 600;
+    }
+  }
+
+  .panelMobi {
     position: absolute;
     bottom: -60%;
     width: 100%;
@@ -53,13 +73,24 @@
   .openP {
     bottom: 0;
   }
+
+  .rightSideTransition {
+    transform: translateX(-100%);
+  }
 }
 </style>
 
 <template>
   <div class="calendar">
-    <div class="table" @click="openPanel($event)">
-      <div v-for="(day, dayIndex) in week" class="row" :key="dayIndex">
+    <div class="table">
+      <!-- 添加@contextmenu.prevent禁用右键点击事件 -->
+      <div
+        v-for="(day, dayIndex) in week"
+        class="row"
+        :key="dayIndex"
+        @contextmenu.prevent
+        @mousedown="openContextMenu($event)"
+      >
         <content
           v-for="(course, timeIndex) in day"
           :key="timeIndex"
@@ -69,8 +100,17 @@
       </div>
     </div>
 
-    <div class="panel" :class="{ openP: isOpenP }">
-      <div class="close" @click="isOpenP = false">关闭</div>
+    <!-- 添加@contextmenu.prevent禁用右键点击事件 -->
+    <div
+      :class="{
+        openP: isOpenP,
+        panelPC: isPC,
+        panelMobi: !isPC,
+        rightSideTransition: isRightSide,
+      }"
+      @contextmenu.prevent
+    >
+      <div class="close" @click="unShowContextMenu">关闭</div>
       <div class="name">{{ ListName + ' ' + timeWord[select.time] }}</div>
       <div class="name">{{ row[select.x] + ' ' + column[select.y - 1] }}</div>
       <div
@@ -124,6 +164,7 @@ const menu: any = {
           from.value.state = states.leave;
           select.type = states.leave;
           showList();
+          unShowContextMenu();
         },
       },
       {
@@ -131,8 +172,8 @@ const menu: any = {
         behavor: () => {
           from.value.name = '取消';
           from.value.state = -1;
-          isOpenP.value = false;
           isTo.value = true;
+          unShowContextMenu();
         },
       },
     ],
@@ -146,6 +187,7 @@ const menu: any = {
           from.value.state = states.study;
           select.type = states.study;
           showList();
+          unShowContextMenu();
         },
       },
     ],
@@ -157,18 +199,67 @@ const select = { x: 0, y: 0, time: times.future, type: states.nothing };
 const from = ref<statesType>({ x: 0, y: 0 });
 //是否打开日程选项
 const isOpenP = ref(false);
+//是否是PC端
+const isPC = ref(false);
+isPC.value = window.innerWidth > 600 ? true : false;
 //是否正在调整日程
 const isTo = ref(false);
+//判断是否是PC端并进行一些打开日程选项的准备工作
+const openContextMenu = (e: MouseEvent) => {
+  if (isOpenP) {
+    unShowContextMenu();
+  }
+  openPanel(e);
+};
+//定位PC端选项菜单的坐标
+let contextMenuTop: Ref<string> = ref('100vh');
+let contextMenuLeft: Ref<string> = ref('100vw');
+//判断菜单是否过于靠右
+let isRightSide: Ref<boolean> = ref(false);
+//获取页面宽高帮助定位
+const windowWidth = document.documentElement.clientWidth;
+const windowHeight = document.documentElement.clientHeight;
+//PC端显示选项菜单
+const showContextMenu = (x: number, y: number) => {
+  // 在指定的位置显示选项菜单
+  if (isPC.value) {
+    contextMenuLeft.value = (x / windowWidth) * 100 + 'vw';
+    contextMenuTop.value = (y / windowHeight) * 100 + 'vh';
+    //如果过于靠右则向左打开，即添加想左平移的class
+    isRightSide.value = (x / windowWidth) * 100 > 80 ? true : false;
+  }
+  //不是PC端则按照原本的程序进行
+  else {
+    isOpenP.value = true;
+  }
+};
+//关闭时取消显示选项菜单，将选项菜单直接定位到可视区域下方
+const unShowContextMenu = () => {
+  isOpenP.value = false;
+  if (isPC.value) {
+    contextMenuLeft.value = '100vw';
+    contextMenuTop.value = '100vh';
+  }
+};
+
 //打开日程选项
 const openPanel = (e: MouseEvent) => {
   isOpenP.value = false;
   const Element = e.target as HTMLElement;
   //如果点击的不是日程表的内容，直接返回
-  if (!Element.classList.contains('content')) return;
+  //并关闭选项菜单
+  if (!Element.classList.contains('content')) {
+    unShowContextMenu();
+    return;
+  }
   select.x = Number(Element.getAttribute('x'));
   select.y = Number(Element.getAttribute('y'));
   //如果点击的是最左侧或者最上侧一栏，直接返回
-  if (!(select.x * select.y)) return;
+  //并关闭选项菜单
+  if (!(select.x * select.y)) {
+    unShowContextMenu();
+    return;
+  }
   select.time = Number(Element.getAttribute('time'));
   select.type = Number(Element.getAttribute('type'));
   //如果正在调整日程
@@ -178,11 +269,17 @@ const openPanel = (e: MouseEvent) => {
   }
   //如果选中的是已经结束的日程，不展示选项，仅展示基础信息
   if (select.time != times.future) {
+    if (isPC.value && e.button == 2) {
+      showContextMenu(e.x, e.y);
+    }
     showBase();
     return;
   }
   from.value = week[select.x][select.y];
   //如果选中的是未开始日程，显示选项
+  if (isPC.value && e.button == 2) {
+    showContextMenu(e.x, e.y);
+  }
   showList();
 };
 const exchangeTo = () => {
